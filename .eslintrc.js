dd31a12a773fcec5b5af0a22da21a4adb2f0e0ca
Sourinda Sivilay <s.sivilay@live.fr>
2021-06-26 00:37:55 +0200

    build: fix all conflicts between eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', 'prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    indent: 'off',
-    'space-before-function-paren': 'off',
   },
 };
